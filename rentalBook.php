869569d8038ff4c6f160007f1379e3581c969c5b
fixlynax <82224707+fixlynax@users.noreply.github.com>
2024-05-25 01:09:22 +0800

    Update rentalBook.php
    update new

--- rentalBook.php
+++ rentalBook.php
@@ -76,7 +76,7 @@ $total_pages = ceil($total_records / $records_per_page);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Reservation History</title>
+    <title>Book Rental</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="./custom_css/navbar.css">
     <link rel="stylesheet" href="./custom_css/layout.css">
